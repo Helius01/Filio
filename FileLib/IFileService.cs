96ec8b4a0f51a905c0d93b378baf3ce098b0d340
kalepook <kooroosh.kabir1@gmail.com>
2023-12-30 13:06:35 +0330

    feat : signed functions in IFIleService

--- FileLib/IFileService.cs
+++ FileLib/IFileService.cs
@@ -2,5 +2,9 @@
 
 public interface IFileService
 {
-    
+    Task UploadAsync(Stream fileStream, string bucket, string path);
+
+    Task DeleteAsync(string bucket, string path);
+
+    string GetPublicUrl(string bucket, string path);
 }
