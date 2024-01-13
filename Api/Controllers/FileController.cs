77cad188840c548d3740577ee07126def42469e3
kalepook <kooroosh.kabir1@gmail.com>
2024-01-13 18:00:53 +0330

    fix : improved file controller

--- Api/Controllers/FileController.cs
+++ Api/Controllers/FileController.cs
@@ -1,9 +1,11 @@
-using Amazon.S3;
-using Filio.Api.Models.RestApi.Upload;
+using Filio.Api.Data;
+using Filio.Api.Models.RestApi.Get;
+using Filio.Common.FileDetector;
+using Filio.Common.ImageLib;
 using Filio.FileLib;
-using Filio.FileLib.Settings.Aws;
-using Microsoft.AspNetCore.Authorization;
+using Filio.FileLib.Models.Get;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Filio.Api.Controllers;
 
@@ -15,16 +17,57 @@ namespace Filio.Api.Controllers;
 public partial class FileController : ControllerBase
 {
     private readonly IFileService _fileService;
-    private readonly AwsSettings _settings;
+    private readonly DataContext _context;
+    private readonly IFileDetectorService _fileDetectorService;
+    private readonly IImageLibService _imageLibService;
 
     /// <summary>
-    /// 
+    /// Default constructor
     /// </summary>
     /// <param name="fileService"></param>
-    /// <param name="settings"></param>
-    public FileController(IFileService fileService, AwsSettings settings)
+    /// <param name="imageLibService"></param>
+    /// <param name="context"></param>
+    /// <param name="fileDetectorService"></param>
+    public FileController(IFileService fileService, IImageLibService imageLibService, DataContext context, IFileDetectorService fileDetectorService)
     {
         _fileService = fileService;
-        _settings = settings;
+        _imageLibService = imageLibService;
+        _context = context;
+        _fileDetectorService = fileDetectorService;
+    }
+
+    /// <summary>
+    /// Returns the file url 
+    /// </summary>
+    /// <param name="id">File id in GUID</param>
+    /// <param name="cancellationToken"></param>
+    /// <response code="200">Success : SingleGetResponse</response>
+    /// <response code="404">Error : File not found</response>
+    /// <returns></returns>
+    [HttpGet("single/{id}")]
+    [ProducesResponseType(typeof(SingleGetResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var file = await _context.FileDomains.Where(x => x.Id == id && !x.IsDeleted)
+                                            .AsNoTracking()
+                                            .FirstOrDefaultAsync(cancellationToken);
+
+        if (file is null)
+        {
+            return NotFound("Couldn't find the file");
+        }
+
+        var publicUrl = _fileService.GetPublicUrl(new SingleGetInput(bucket: file.BucketName, path: file.Path));
+        var signedUrl = _fileService.GetSignedUrl(new SingleGetInput(bucket: file.BucketName, path: file.Path));
+
+        var response = new SingleGetResponse(signedUrl: signedUrl,
+                                               publicUrl: publicUrl,
+                                               bucket: file.BucketName,
+                                               metadata: file.MetaData,
+                                               imageBlurhash: file.ImageBlurhash,
+                                               type: file.Type);
+
+        return Ok(response);
     }
 }
--- Api/Controllers/Single.FileController.cs
+++ Api/Controllers/Single.FileController.cs
@@ -1,7 +1,15 @@
-using Filio.Api.Models.RestApi.Delete;
+using System.Net;
+using Filio.Api.Data;
+using Filio.Api.Domains;
 using Filio.Api.Models.RestApi.Get;
 using Filio.Api.Models.RestApi.Upload;
+using Filio.Common.ErrorHandler;
+using Filio.Common.ErrorHandler.RecoverableErrors;
+using Filio.Common.FileDetector;
+using Filio.FileLib.Models.Delete;
+using Filio.FileLib.Models.Upload;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Filio.Api.Controllers;
 
@@ -11,55 +19,115 @@ public partial class FileController
     /// Uploads a single file 
     /// </summary>
     /// <param name="request"></param>
-    /// <response code="400">Error:Wrong request or invalid data</response>
+    /// <response code="500">Error: Panic</response>
     /// <response code="404">Error:Usually on wrong bucket name</response>
-    /// <response code="200">Success/response>
+    /// <response code="400">Error:Wrong request or invalid data</response>
+    /// <response code="200">Success: SingleUploadResponse</response>
     /// <returns></returns>
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(SingleUploadResponse), StatusCodes.Status200OK)]
     [HttpPost("single")]
     public async Task<IActionResult> Upload([FromForm] SingleUploadRequest request)
     {
         using var fileStream = request.File.OpenReadStream();
 
-        var result = await _fileService.UploadAsync(fileStream, request.BucketName, request.Path);
+        var fileType = _fileDetectorService.GetType(fileStream);
 
-        return result.IsSuccess ? Ok(new SingleUploadResponse { PublicUrl = result.Value.PublicUrl, SignedUrl = result.Value.SignedUrl })
-                                : result.Error!.StatusCode switch
-                                {
-                                    System.Net.HttpStatusCode.NotFound => NotFound(new { result.Error.Message }),
-                                    System.Net.HttpStatusCode.BadRequest => BadRequest(new { result.Error.Message }),
-                                    _ => Problem(detail: result.Error.Message, statusCode: (int)result.Error.StatusCode)
-                                };
+        if (fileType == FileType.None)
+        {
+            //TODO:Log error
+            //TODO:Use strong type for error response
+            return BadRequest(new { Error = "File type not supported" });
+        }
+
+        var blurhash = string.Empty;
+
+        if (fileType == FileType.Image)
+        {
+            //Generating blurhash
+            blurhash = await _imageLibService.GenerateBlurhashAsync(fileStream);
+        }
+
+        var newFile = new FileDomain(bucketName: request.BucketName,
+                                    sizeInByte: request.File.Length,
+                                    extension: Path.GetExtension(request.File.FileName),
+                                    originalName: request.File.FileName,
+                                    type: request.File.ContentType);
+
+        if (!string.IsNullOrEmpty(blurhash))
+        {
+            newFile.UpdateBlurhash(blurhash);
+        }
+
+        //TODO:It doesn't works we needs creates a serializable dictionary in form mode
+        if (request.MetaData != null && request.MetaData.Count > 0)
+        {
+            newFile.UpdateMetadata(request.MetaData);
+        }
+
+
+        _context.FileDomains.Add(newFile);
+
+
+        Either<HttpError, SingleUploadOutput> result = new();
+
+        await ResilientTransaction.Create(_context).ExecuteAsync(async () =>
+        {
+            await _context.SaveChangesAsync();
+
+            result = await _fileService.UploadAsync(new SingleUploadInput(stream: fileStream,
+                                                                            path: newFile.Path,
+                                                                            bucket: newFile.BucketName,
+                                                                        metadata: request.MetaData));
+        });
+
+
+        return result.Match<IActionResult>(
+            left => left.StatusCode switch
+            {
+                HttpStatusCode.BadRequest => BadRequest(new { Error = left.Message }),
+                HttpStatusCode.NotFound => NotFound(new { Error = left.Message }),
+                _ => Problem(detail: left.Message, statusCode: (int)left.StatusCode)
+            }
+        , right => Ok(new SingleUploadResponse
+        {
+            FileId = newFile.Id,
+            PublicUrl = right.PublicUrl,
+            SignedUrl = right.SignedUrl
+        }));
     }
 
     /// <summary>
     /// Deletes a single file
     /// </summary>
-    /// <param name="request"></param>
+    /// <param name="id">The file id in guid</param>
+    /// <param name="cancellationToken"></param>
+    /// <response code="404">Error : File not found</response>
     /// <response code="204">Success : File deleted</response>
     /// <returns></returns>
-    [HttpDelete("single")]
+    [HttpDelete("single/{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> Delete([FromBody] SingleDeleteRequest request)
+    public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken cancellationToken)
     {
-        await _fileService.DeleteAsync(request.BucketName, request.Path);
+        var file = await _context.FileDomains.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+        if (file is null)
+        {
+            return NotFound(new { Error = "Couldn't find the file" });
+        }
+
+        file.Delete();
+        _context.FileDomains.Update(file);
+
+        await ResilientTransaction.Create(_context).ExecuteAsync(async () =>
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+            await _fileService.DeleteAsync(new SingleDeleteInput(path: file.Path, bucket: file.BucketName), cancellationToken);
+        });
 
         return NoContent();
-    }
-
-    /// <summary>
-    /// Returns the file url 
-    /// </summary>
-    /// <param name="bucket"></param>
-    /// <param name="filePath"></param>
-    /// <response code="200">Success : File urls</response>
-    /// <returns></returns>
-    [HttpGet("single/{bucket}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult Get([FromRoute] string bucket, [FromQuery] string filePath)
-    {
-        return Ok(new SingleGetResponse { SignedUrl = _fileService.GetSignedUrl(bucket, filePath) });
     }
 }
