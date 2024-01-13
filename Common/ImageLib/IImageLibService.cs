807349d503c7f8fb6ad084b00dccaa187a45a26d
kalepook <kooroosh.kabir1@gmail.com>
2024-01-13 13:12:40 +0330

    feat : Added ImageLibService

---
+++ Common/ImageLib/IImageLibService.cs
@@ -0,0 +1,27 @@
+namespace Filio.Common.ImageLib;
+
+/// <summary>
+/// Provides many functions to play with images
+/// </summary>
+public interface IImageLibService
+{
+    /// <summary>
+    /// Generates a blurhash from an image
+    /// </summary>
+    /// <param name="imageStream">The image stream</param>
+    /// <param name="componentsX">X component</param>
+    /// <param name="componentsY">Y component</param>
+    /// <returns></returns>
+    Task<string> GenerateBlurhashAsync(Stream imageStream, int componentsX = 6, int componentsY = 6);
+
+    /// <summary>
+    /// Resizes an image and returns the new stream
+    /// </summary>
+    /// <param name="imageStream"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <param name="quality"></param>
+    /// <param name="removeMetaData"></param>
+    /// <returns></returns>
+    Task<Stream> ResizeAsync(Stream imageStream, int? width = null, int? height = null, int quality = 100, bool removeMetaData = true);
+}
---
+++ Common/ImageLib/ImageLibService.cs
@@ -0,0 +1,61 @@
+using Filio.Common.Blurhash;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace Filio.Common.ImageLib;
+
+internal class ImageLibService : IImageLibService
+{
+    private readonly IBlurhashService _blurhashService;
+
+    public ImageLibService(IBlurhashService blurhashService)
+    {
+        _blurhashService = blurhashService;
+    }
+
+    ///<inheritdoc />
+    public async Task<string> GenerateBlurhashAsync(Stream imageStream, int componentsX = 6, int componentsY = 6)
+    {
+        imageStream.Position = 0;
+
+        using var image = await Image.LoadAsync<Rgb24>(imageStream);
+
+        return _blurhashService.Encode(image, componentsX, componentsY);
+    }
+
+    ///<inheritdoc />
+    public async Task<Stream> ResizeAsync(Stream imageStream, int? width = null, int? height = null, int quality = 100, bool removeMetaData = true)
+    {
+        if (height is null && width is null)
+            throw new ArgumentException("Width and height cannot be null at the same time");
+
+        imageStream.Position = 0;
+
+        using var image = await Image.LoadAsync(imageStream);
+
+        if (removeMetaData)
+            image.Metadata.ExifProfile = null;
+
+        var resizeOptions = new ResizeOptions
+        {
+            Compand = false,
+            Mode = ResizeMode.Crop,
+            Position = AnchorPositionMode.Center,
+            Size = new Size(width ?? 0, height ?? 0)
+        };
+
+        image.Mutate(x => x.Resize(resizeOptions));
+
+        using var newStream = new MemoryStream();
+
+        image.Save(newStream, new JpegEncoder
+        {
+            Quality = quality,
+            ColorType = JpegEncodingColor.YCbCrRatio444
+        });
+
+        return newStream;
+    }
+}
