c0ffbaa6947fa0c6dbf8508b99029d150fd9c802
kalepook <kooroosh.kabir1@gmail.com>
2024-01-22 13:41:37 +0330

    test

---
+++ Tests/FileDomainTests.cs
@@ -0,0 +1,32 @@
+using Filio.Api.Domains;
+using Filio.Api.Exceptions;
+
+namespace Tests;
+
+public class FileDomainTests
+{
+    [Theory]
+    [InlineData("fa")]//Too short
+    [InlineData("MYBUCKET")]//Full UPPER case
+    [InlineData("Mybucket")]//Contains UPPER case character
+    [InlineData("192.168.1.1")]//Can't like an IP Address
+    [InlineData("xn--mybucket")]//Bad Prefix
+    [InlineData("sthree-mybucket")]//Bad Prefix
+    [InlineData("sthree-configurator.mybucket")]//BadPrefix
+    [InlineData("mybucket-s3alias")]//Bad suffix
+    [InlineData("mybucket--ol-s3")]//Bad Suffix
+    [InlineData("mybucket.first")]//Dot Contains
+    [InlineData("VsMC6AVLfXC0syNyD6rLUaVCJBZMDz9dlKMhWfAwCPiXVAyWbxU32KnlABRZADFd")]//Too long (64) length
+
+    public void CreatingFile_WithInvalidBucketName_ShouldException(string bucketName)
+    {
+
+        Assert.Throws<DomainException>(() => new FileDomain(bucketName: "",
+                                    sizeInByte: 10,
+                                    extension: ".dummy",
+                                    originalName: "dummy.dummy",
+                                    FileDomainType.Video,
+                                    imageBlurhash: null));
+
+    }
+}
